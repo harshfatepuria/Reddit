9ffeef276aefd065a71230203a314dd65b527a96
Harsh Fatepuria <fatepuri@usc.edu>
2016-09-15 19:49:31 -0700

    Added code to fetch submissions and comments by a user

--- reddit_mining.py
+++ reddit_mining.py
@@ -3,13 +3,15 @@ import pprint
 import operator
 pp=pprint.PrettyPrinter(indent=4)
 
+r = praw.Reddit(user_agent='new_application_crawler_reddit')
+
 def get_authors_of_comments_on_submission_about_subreddit(subreddit):
 	authors = dict()
-	r = praw.Reddit(user_agent='new_application_crawler_reddit')
 	len_submissions = 50
 	submissions = r.get_subreddit(subreddit).get_top(limit = len_submissions)
 	i = 1
 	for submission in submissions:
+		submission.replace_more_comments(limit=None, threshold=0)
 		print "\n\n",i,") Submission Title: ",vars(submission)['title']
 		all_comments = submission.comments
 
@@ -35,5 +37,28 @@ def get_authors_of_comments_on_submission_about_subreddit(subreddit):
 	sorted_authors = sorted(authors.items(), key=operator.itemgetter(1), reverse=True)
 	return sorted_authors
 
+def get_submissions_by_a_user(user):
+	user = r.get_redditor(user)
+	submissions = user.get_submitted(limit=None)
+	print "# submissions by",user,"=",
+
+	len_submissions = 0
+	for submission in submissions:
+		len_submissions = len_submissions + 1
+	print len_submissions
+
+def get_comments_by_a_user(user):
+	user = r.get_redditor(user)
+	comments = user.get_comments(limit=None)
+	print "# comments by",user,"=",
+	
+	len_comments = 0
+	for comment in comments:
+		len_comments = len_comments + 1
+	print len_comments
+
+
 sorted_authors_of_subreddit = get_authors_of_comments_on_submission_about_subreddit('marvel')
-print pp.pprint(sorted_authors_of_subreddit)
+pp.pprint(sorted_authors_of_subreddit)
+get_submissions_by_a_user(sorted_authors_of_subreddit[0][0])
+get_comments_by_a_user(sorted_authors_of_subreddit[0][0])
