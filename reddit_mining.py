09592649da8233e55c58ac00ecea3f9105767e31
Harsh Fatepuria <fatepuri@usc.edu>
2016-09-17 10:47:30 -0700

    changes

--- reddit_mining.py
+++ reddit_mining.py
@@ -7,11 +7,10 @@ r = praw.Reddit(user_agent='new_application_crawler_reddit')
 
 def get_authors_of_comments_on_submission_about_subreddit(subreddit):
 	authors = dict()
-	len_submissions = 50
-	submissions = r.get_subreddit(subreddit).get_top(limit = len_submissions)
+	submissions = r.get_subreddit(subreddit).get_top(limit = None)
 	i = 1
 	for submission in submissions:
-		submission.replace_more_comments(limit=None, threshold=0)
+		submission.replace_more_comments(limit = None, threshold = 0)
 		print "\n\n",i,") Submission Title: ",vars(submission)['title']
 		all_comments = submission.comments
 
@@ -27,12 +26,14 @@ def get_authors_of_comments_on_submission_about_subreddit(subreddit):
 
 		for comment in all_comments:
 			json_comment = vars(comment)
-			json_author = vars(json_comment['author'])
-			author = str(json_author['name'])
-			if author in authors:
-				authors[author] = authors[author] + 1
-			else:
-				authors[author] = 1
+
+			if json_comment['author']:
+				json_author = vars(json_comment['author'])
+				author = str(json_author['name'])
+				if author in authors:
+					authors[author] = authors[author] + 1
+				else:
+					authors[author] = 1
 		i = i + 1
 	sorted_authors = sorted(authors.items(), key=operator.itemgetter(1), reverse=True)
 	return sorted_authors
@@ -73,33 +74,36 @@ def get_subreddits_commented_on_by_a_set_of_users(subreddit , list_of_users_tupl
 	for user_tuple in list_of_users_tuple:
 		username = user_tuple[0]
 		list_of_subreddits_tuple = get_comments_by_a_user(username)
-		print i,"of",number_of_users,") has commented on",len(list_of_subreddits_tuple),"unique subreddits"
+		print '\n',i,"of",number_of_users,") has commented on",len(list_of_subreddits_tuple),"unique subreddits",
 		for subreddit_tuple in list_of_subreddits_tuple:
 			subreddit_name = subreddit_tuple[0]
 			if subreddit_name in generic_subreddit_commented_on_by_all_users:
 				generic_subreddit_commented_on_by_all_users[subreddit_name] = generic_subreddit_commented_on_by_all_users[subreddit_name] + subreddit_tuple[1]
 			else:
 				generic_subreddit_commented_on_by_all_users[subreddit_name] = subreddit_tuple[1]
-		i = i + 1
-		if i % 31 == 0:
+		if i % 20 == 0:
 			output_file=open('subreddits_related_to_' + subreddit + '_' + str(i) + '.txt','w')
-			output_file.write(str(generic_subreddit_commented_on_by_all_users))
+			output_file.write(str(sorted(generic_subreddit_commented_on_by_all_users.items(), key=operator.itemgetter(1), reverse=True)))
 			output_file.close()
+		i = i + 1
 	output_file=open('subreddits_related_to_' + subreddit + '_all.txt','w')
-	output_file.write(str(generic_subreddit_commented_on_by_all_users))
+	output_file.write(str(sorted(generic_subreddit_commented_on_by_all_users.items(), key=operator.itemgetter(1), reverse=True)))
 	output_file.close()
 	
+input_file=open('subreddits.txt','r')
+subreddits=input_file.read().split("\n")
 
+for subreddit in subreddits:
+	print '\n\n',subreddit
+	print '=================='
+	sorted_authors_of_subreddit = get_authors_of_comments_on_submission_about_subreddit(subreddit)
+	sorted_authors_of_subreddit=sorted_authors_of_subreddit[:len(sorted_authors_of_subreddit)/2]
+	output_file=open('top_commenters_of_' + subreddit + '.txt','w')
+	output_file.write(str(sorted_authors_of_subreddit))
+	output_file.close()
 
-subreddit='marvel'
-sorted_authors_of_subreddit = get_authors_of_comments_on_submission_about_subreddit(subreddit)
+	# input_file=open('top_commenters_of_' + subreddit + '.txt','r')
+	# sorted_authors_of_subreddit=eval(input_file.read())
+	# input_file.close()
 
-output_file=open('all_commenters_of_' + subreddit + '.txt','w')
-output_file.write(str(sorted_authors_of_subreddit))
-output_file.close()
-
-input_file=open('all_commenters_of_' + subreddit + '.txt','r')
-sorted_authors_of_subreddit_new=eval(input_file.read())
-input_file.close()
-
-get_subreddits_commented_on_by_a_set_of_users(subreddit , sorted_authors_of_subreddit_new)
+	get_subreddits_commented_on_by_a_set_of_users(subreddit , sorted_authors_of_subreddit)
